8329992c6807c0bd920db284e7df4710137325bb
Sanda <sandanarotama362@gmail.com>
2025-08-22 17:50:54 +0700

    Update menambahkan anggota bisa minjam(masih error dikir)

--- public/clear-cache.php
+++ public/clear-cache.php
@@ -5,8 +5,8 @@
 
 use Illuminate\Support\Facades\Artisan;
 
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 
