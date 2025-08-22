ade7cb71007d4ea5c904e87249efa21a53e482cd
Sanda <sandanarotama362@gmail.com>
2025-08-22 17:58:33 +0700

    Update

--- public/clear-cache.php
+++ public/clear-cache.php
@@ -1,15 +1,28 @@
 <?php
-use Illuminate\Support\Facades\Artisan;
+
+use Illuminate\Contracts\Console\Kernel;
 
 require __DIR__.'/../vendor/autoload.php';
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+// Bootstrap kernel dulu
+$kernel = $app->make(Kernel::class);
+$kernel->bootstrap();
 
 echo "<pre>";
-Artisan::call('config:clear'); echo Artisan::output();
-Artisan::call('cache:clear');  echo Artisan::output();
-Artisan::call('route:clear');  echo Artisan::output();
-Artisan::call('view:clear');   echo Artisan::output();
-echo "✅ Cache Cleared!";
+
+\Artisan::call('config:clear');
+echo \Artisan::output();
+
+\Artisan::call('cache:clear');
+echo \Artisan::output();
+
+\Artisan::call('route:clear');
+echo \Artisan::output();
+
+\Artisan::call('view:clear');
+echo \Artisan::output();
+
+echo "✅ Semua cache berhasil dibersihkan!";
 echo "</pre>";
